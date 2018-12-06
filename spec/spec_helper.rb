7c98e655cd643c13129861d17d39aeea459cc7bd
Fernando Aleman <fernandoaleman@mac.com>
2018-12-06 15:03:36 -0600

    Remove rubocop customizations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,16 +4,14 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 SUPPORTED_PLATFORMS = {
-  # rubocop:disable Style/WordArray
-  'centos' => ['7'],
-  'debian' => ['8', '9'],
-  'fedora' => ['27', '28'],
-  'ubuntu' => ['14.04', '16.04', '18.04']
-  # rubocop:enable Style/WordArray
+  'centos' => %w(7),
+  'debian' => %w(8 9),
+  'fedora' => %w(27 28),
+  'ubuntu' => %w(14.04 16.04 18.04),
 }.freeze
 
 RSpec.configure do |config|
-  config.color = true
+  config.color     = true
   config.formatter = :documentation
   config.log_level = :error
 end
