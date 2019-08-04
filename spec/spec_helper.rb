2d74efd4830f59e110ef9a7b687ce3b88d571bac
Fernando Alemán <fernandoaleman@mac.com>
2019-08-04 16:31:07 -0500

    Update rspec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,7 @@
-# frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
-
-# rubocop:disable Style/WordArray
-SUPPORTED_PLATFORMS = {
-  'amazon' => ['2'],
-  'centos' => ['7'],
-  'debian' => ['8', '9'],
-  'fedora' => ['27', '28'],
-  'ubuntu' => ['16.04', '18.04'],
-}.freeze
 
 RSpec.configure do |config|
   config.color     = true
   config.formatter = :documentation
-  config.log_level = :error
 end
